3664964bad927ce1007fda96ff0115197baf728a
femkegoos <femke.goos.01@gmail.com>
2026-05-29 21:32:07 +0200

    Add tab navigation and font loading to App component

--- BA-app/App.js
+++ BA-app/App.js
@@ -28,3 +28,44 @@ function HomeStack() {
     </Stack.Navigator>
   );
 }
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'PoppinsRegular': require('./assets/fonts/Poppins-Regular.ttf'),
+    'PoppinsBold': require('./assets/fonts/Poppins-Bold.ttf'),
+    'PoppinsSemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+    'PoppinsItalic': require('./assets/fonts/Poppins-Italic.ttf'),
+
+  });
+    if (!fontsLoaded) {
+    return null;
+    }
+    return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+            headerShown: false,
+            tabBarShowIcon: false,
+            tabBarActiveTintColor: '#ffff',
+            tabBarInactiveTintColor: 'rgba(255, 255, 255, 0.7)',
+            tabBarStyle: {
+              backgroundColor: '#f7fcee',
+              borderTopColor: '#f7fcee',
+                paddingBottom: 8,
+                height: 72,
+            },
+            tabBarLabelStyle: {
+              fontSize: 14,
+                fontFamily: 'PoppinsRegular',
+                fontSize: 18,
+            },
+        }}
+        >
+        <Tab.Screen name="Studieaanbod" component={HomeStack} options={{tabBarIcon: ()=> null,}} />
+        <Tab.Screen name="Nieuws" component={HomeStack} options={{tabBarIcon: ()=> null,}} />
+        <Tab.Screen name="Over BA" component={HomeStack} options={{tabBarIcon: ()=> null,}} />   
+            <Tab.Screen name="Inschrijven" component={HomeStack} options={{tabBarIcon: ()=> null,}} />   
+        </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
