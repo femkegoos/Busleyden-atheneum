00380aef51730c5655a01dcecd9bb3aa861283e5
femkegoos <femke.goos.01@gmail.com>
2026-05-31 23:14:36 +0200

    Implement CampusDetail screen with image and detailed information layout

--- BA-app/screens/CampusDetail.js
+++ BA-app/screens/CampusDetail.js
@@ -1,9 +1,69 @@
-import { View, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 
-export default function CampusDetail() {
+const CampusDetail = ({ route }) => {
+  const { name, focus, beschrijving, adres, gemeente, kleur, image } = route.params;
+
   return (
-    <View>
-      <Text>CampusDetails</Text>
-    </View>
+    <ScrollView style={styles.container}>
+
+      {/* Afbeelding */}
+      <Image source={image} style={styles.image} />
+
+      {/* Info */}
+      <View style={styles.info}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.adres}>{adres}</Text>
+        <Text style={styles.gemeente}>{gemeente}</Text>
+        <Text style={[styles.focus, { color: kleur }]}>{focus}</Text>
+        <Text style={styles.beschrijving}>{beschrijving}</Text>
+
+      </View>
+
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  image: {
+    width: '100%',
+    height: 250,
+  },
+  info: {
+    padding: 24,
+  },
+  name: {
+    fontSize: 38,
+    fontFamily: 'PoppinsBold',
+    marginTop: 8,
+  },
+  focus: {
+    fontSize: 16,
+    fontFamily: 'PoppinsSemiBold',
+    marginTop: 24,
+  },
+  beschrijving: {
+    fontSize: 14,
+    fontFamily: 'PoppinsRegular',
+    color: '#555',
+    marginTop: 8,
+    lineHeight: 22,
+  },
+  adres: {
+    fontSize: 14,
+    fontFamily: 'PoppinsItalic',
+    color: '#555',
+    marginTop: 0,
+  },
+  gemeente: {
+    fontSize: 14,
+    fontFamily: 'PoppinsItalic',
+    color: '#555',
+    marginTop: -4,
+  },
+});
+
+export default CampusDetail;
